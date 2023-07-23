23900589dc2b024a8a5206c8e1e9759a83e7230f
EngineerSk <eskayoriade@gmail.com>
2023-07-23 15:32:04 +0100

    unused import removed

--- app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
+++ app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.map
-import androidx.lifecycle.switchMap
 import java.text.NumberFormat
 import java.text.SimpleDateFormat
 import java.util.Calendar
